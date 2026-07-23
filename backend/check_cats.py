54dd4b77ee51f05323d366bb6b7e4351c8705e99
Hamdan <hamdan@asiansol.com>
2026-07-23 20:36:43 +0500

    fix: new gmail app password, navbar links centered, TRENDZY text logo, favicon PNG

--- backend/check_cats.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('trendzy.db')
-cur = conn.cursor()
-cur.execute('SELECT name FROM categories ORDER BY "order"')
-print([r[0] for r in cur.fetchall()])
-conn.close()
